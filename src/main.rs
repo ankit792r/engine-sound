5a6cbc4f97ed68533a1c57e7b0465e0db13c9176
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-05-16 00:07:40 +0530

    combined all the oscillators

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,30 @@ use oscillator::oscillator_v1::Oscilltor;
 
 fn main() {
     let host = cpal::default_host();
-    let device = host.default_output_device().unwrap();
+    let device = host.default_output_device().expect("No output device");
 
-    let config = device.default_output_config().unwrap();
+    let config = device.default_output_config().expect("No default config");
 
     let sample_rate = config.sample_rate() as f32;
 
-    let mut osc = Oscilltor {
-        phase: 0.0,
-        frequency: 440.0,
-        sample_rate,
-    };
+    let mut oscillators = vec![
+        Oscilltor::new(110.0, sample_rate),
+        Oscilltor::new(220.0, sample_rate),
+        Oscilltor::new(330.0, sample_rate),
+    ];
 
     let stream = device
         .build_output_stream(
             &config.into(),
             move |data: &mut [f32], _| {
                 for sample in data.iter_mut() {
-                    *sample = osc.next_sample() * 0.1;
+                    let mut mixed = 0.0;
+
+                    for osc in oscillators.iter_mut() {
+                        mixed += osc.next_sample() * 0.2;
+                    }
+
+                    *sample = mixed;
                 }
             },
             move |err| {
